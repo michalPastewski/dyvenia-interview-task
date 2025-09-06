136bfb777ed7caaf67428a439c80a1eaf46e6406
MPastewski <michpastewski@gmail.com>
2025-09-06 17:23:56 +0200

    init commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Chatbot from './components/chatbot/chatbot';
+import CodeEditor from './components/codeEditor';
+import Footer from './components/footer';
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="px-2 pt-2 flex flex-col h-screen bg-[#9484e3]">
+    
+    </div>
+  );
+}
+
+export default App;
